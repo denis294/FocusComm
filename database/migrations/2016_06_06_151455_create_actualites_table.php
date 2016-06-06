24a51cac4ae1d1769a6e9ec289904f08693bbc92
SanSolo <gaellelehmann@nilslehmann.home>
2016-06-06 17:41:03 +0200

    Migration Table Actualites

---
+++ database/migrations/2016_06_06_151455_create_actualites_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateActualitesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('actualites', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('titre', 80);
+            $table->date('dateCreation');
+            $table->unique(['titre', 'dateCreation']);
+            $table->text('texte');
+            $table->string('image');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->integer('updated_by')->unsigned()->nullable();
+            $table->foreign('updated_by')->references('id')->on('users');
+            $table->integer('categorie_id')->unsigned();
+            $table->foreign('categorie_id')->references('id')->on('categories');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+         Schema::drop('actualites');
+    }
+}
