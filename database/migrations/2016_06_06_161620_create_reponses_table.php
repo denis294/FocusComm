7feb58a06c312e29addb9cd6ed5408e3c214fdda
SanSolo <gaellelehmann@nilslehmann.home>
2016-06-06 18:24:57 +0200

    Migration Table Reponses

---
+++ database/migrations/2016_06_06_161620_create_reponses_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReponsesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reponses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('texte');
+            $table->boolean('estJuste');
+            $table->integer('question_id')->unsigned();
+            $table->foreign('question_id')->references('id')->on('questions');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('reponses');
+    }
+}
