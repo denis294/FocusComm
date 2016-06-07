6008345df53697039e21d43dcc233f4ab582bf65
Guillaume <guillaume.stark@heig-vd.ch>
2016-06-07 10:52:36 +0200

    Modèle badge

---
+++ app/Models/Badge.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Badge extends Model
+{
+    public function quizz()
+    {
+      return $this->belongsTo('Quizz');
+    }
+    
+    public function users()
+    {
+      return $this->belongsToMany('User');
+    }
+}
