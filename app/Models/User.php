0a1f42dac853b6363bc06341b1fc186db039810a
Guillaume <guillaume.stark@heig-vd.ch>
2016-06-07 11:26:42 +0200

    Modèle User
    Correction avec la table intermédiaire

--- app/Models/User.php
+++ app/Models/User.php
@@ -29,7 +29,7 @@ class User extends Model
    
     public function badges()
     {
-        return $this->belongsToMany('Badge');
+        return $this->belongsToMany('Badge', 'badge_user')->withTimestamps();
     }
     
     public function reponses()
