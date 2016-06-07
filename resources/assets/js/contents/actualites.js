46d7348b2e412c4d118267eee046d81cdca84516
denis294 <denis.haedrich@gmail.com>
2016-06-07 09:34:58 +0200

    ajout-fichier-media

---
+++ resources/assets/js/contents/actualites.js
@@ -0,0 +1,21 @@
+var ACTUALITES = [
+
+    {
+        "title": "news1",
+        "description": "il était un petit navirrrrr",
+        "date": "13-12-16",
+        "media": "media1.png"
+    },
+        {
+        "title": "news2",
+        "description": "fatigue, faim,.fsdfdsas ",
+        "date": "14-09-16",
+        "media": "media2.png"
+    },
+        {
+        "title": "news3",
+        "description": "weeeeekeeeend !!!! fdsafdafs",
+        "date": "02-05-16",
+        "media": "media3.png"
+    }
+];
