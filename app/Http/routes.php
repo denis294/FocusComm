878762d338bcb3aef6e089dceeb5df1ad8a58b2c
Guillaume <guillaume.stark@heig-vd.ch>
2016-06-08 15:27:08 +0200

    Quizz Controller
    Première version. Manque interaction avec Quizz et Réponses.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,9 +11,17 @@
 |
 */
 
+// Accueil
 Route::get('/', function () {
     return view('welcome');
 });
+
+// User
+Route::get('/user/', 'UserController@index');            
+Route::get('/user/{id}', 'UserController@show');
+Route::post('/user/', 'UserController@store');
+Route::put('/user/{id}', 'UserController@update');
+Route::delete('/user/{id}', 'UserController@destroy'); 
 
 // Actualité
 Route::get('/actualites/', 'ActualiteController@index');
@@ -24,14 +32,20 @@ Route::get('/actualites/{id}/edit', 'ActualiteController@edit');
 Route::put('/actualites/{id}', 'ActualiteController@update');
 Route::delete('/actualites/{id}', 'ActualiteController@destroy');
 
-Route::get('/auth/login', 'AuthController@login'); 
+// Quizz
+Route::get('/quizzs/', 'QuizzController@index');
+Route::get('/quizzs/categorie/{id}', 'QuizzController@indexQuizz');
+Route::get('/quizzs/create', 'QuizzController@create'); 
+Route::post('/quizzs/', 'QuizzController@store');            
+Route::get('/quizzs/{id}', 'QuizzController@show');
+Route::get('/quizzs/{id}/edit', 'QuizzController@edit');
+Route::put('/quizzs/{id}', 'QuizzController@update');
+Route::delete('/quizzs/{id}', 'QuizzController@destroy');
 
+// Login et logout
+Route::get('/auth/login', 'AuthController@login'); 
 Route::group(['middleware' => ['auth']], function () {    
         Route::get('/auth/logout', 'AuthController@logout');
 });
-Route::get('/user/', 'UserController@index');            
-Route::get('/user/{id}', 'UserController@show');
-Route::post('/user/', 'UserController@store');
-Route::put('/user/{id}', 'UserController@update');
-Route::delete('/user/{id}', 'UserController@destroy');  
+ 
 
