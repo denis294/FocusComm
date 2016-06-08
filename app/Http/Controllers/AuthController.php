6c21860b95502522ec5ee85c563871f6509e1ba1
Guillaume <guillaume.stark@heig-vd.ch>
2016-06-08 08:59:52 +0200

    User -> Pseudo

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -14,20 +14,20 @@ class AuthController extends Controller
 	public function login(){
   		$password = Request::input('password', '');
   		$email = Request::input('email', '');
-  		$user = User::where("email", $email)->first();
+  		$pseudo = User::where("email", $email)->first();
   		
   		// Regarde si le User existe
-  		if (!isset($user)) {
+  		if (!isset($pseudo)) {
       		return 'Connexion échouée, utilisateur inexistant';
   		}
   		
   		// Vérifie le password et le hash
-  		if (!Hash::check($password, $user->password)) {
+  		if (!Hash::check($password, $pseudo->password)) {
       		return 'Connexion échouée, mauvais password';
   		}
   		
   		// Auth persistance,si user existant et bon mot de passe stock l’id du user dans session
-  		Session::put('user_id', $user->id);
+  		Session::put('user_id', $pseudo->id);
   		return 'Connexion réussie';
 	}
 	
