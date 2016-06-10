f68d4071816acbfffc6c7d68463892fc0197db80
Guillaume <guillaume.stark@heig-vd.ch>
2016-06-10 12:52:40 +0200

    packjs
    correction packJs

--- public/assets/js/packJs.php
+++ public/assets/js/packJs.php
@@ -40,7 +40,7 @@ header("Content-type: application/javascript");
         'views/actualitesAdm.js',
 
         // TP
-        //'tp/main.js',
+        'tp/main.js',
         'tp/mainAdm.js',
 
         // AUTRES
