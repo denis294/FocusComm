4cfa0b4ef160ddc72860c18fda998d3b0f8778f6
MrSmon <simon.contrerasaguza@gmail.com>
2016-06-13 16:47:10 +0200

    gestion des article liés
    test divers

---
+++ public/assets/js/test.js
@@ -0,0 +1,14 @@
+
+$(".articleliee").data("actualiteLiee_id");
+
+for(var i = 0; i < NEWS.length; i++)
+{
+  if(NEWS[i].id == 2)
+  {
+    var a = (NEWS[i].titre);
+  }
+}
+console.log($(".articleliee").data("actualiteLiee_id"));
+
+
+
