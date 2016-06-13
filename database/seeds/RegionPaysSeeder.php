df10de0154a8e6978f07c14640e165ebae535f9e
Guillaume <guillaume.stark@heig-vd.ch>
2016-06-13 20:44:08 +0200

    Mis à jour du Seeder et des tables
    Ajout des catégories dans la base de données à quelques changements
    divers dans le seeder

--- database/seeds/RegionPaysSeeder.php
+++ database/seeds/RegionPaysSeeder.php
@@ -102,21 +102,107 @@ class RegionPaysSeeder extends Seeder
         *	Seeding Categories
         */
 
+        $stress = Categorie::create([
+        	'nom' => 'Stress',
+        	'icone' => 'stress.png',
+        ]);
+        
+        $estime = Categorie::create([
+        	'nom' => 'Estime de soi',
+        	'icone' => 'estime.png',
+        ]);
+        
+        $sexualite = Categorie::create([
+        	'nom' => 'Sexualité',
+        	'icone' => 'sexualite.png',
+        ]);
+        
+        $bfsd = Categorie::create([
+        	'nom' => 'Boire, fumer, se droguer',
+        	'icone' => 'bfsd.png',
+        ]);
+        
         $sante = Categorie::create([
-        	'nom' => 'sante',
-        	'icone' => 'sante.jpg',
-        	'description' => 'Comment mieux vivre plus sainement.',
+        	'nom' => 'Santé',
+        	'icone' => 'sante.png',
+        ]);
+        
+        $mangerBouger = Categorie::create([
+        	'nom' => 'Manger-bouger',
+        	'icone' => 'mangerBouger.png',
+        ]);
+        
+        $mtla = Categorie::create([
+        	'nom' => 'Moi, toi et les autres',
+        	'icone' => 'mtla.png',
+        ]);
+        
+        $violences = Categorie::create([
+        	'nom' => 'Violences',
+        	'icone' => 'violences.png',
+        ]);
+        
+        $formationTravail = Categorie::create([
+        	'nom' => 'Formation et travail',
+        	'icone' => 'formationTravail.png',
+        ]);
+        
+        $argent = Categorie::create([
+        	'nom' => 'Argent',
+        	'icone' => 'argent.png',
+        ]);
+        
+        $religionCroyance = Categorie::create([
+        	'nom' => 'Religion et croyance',
+        	'icone' => 'religionCroyance.png',
+        ]);
+        
+        $discriminationRacisme = Categorie::create([
+        	'nom' => 'Discrimination et racisme',
+        	'icone' => 'discriminationRacisme.png',
         ]);
 
-        $manger = Categorie::create([
-        	'nom' => 'manger',
-        	'icone' => 'manger.jpg',
-        	'description' => 'Manger est important, bouger aussi.',
-        ]);
-        // Sida sous-catégorie de Santé
+        // Sous-catégories de Sante
         $sida = Categorie::create([
-        	'nom' => 'sida',
+        	'nom' => 'Sida',
         	'categorieParente_id' => $sante->id,
+        ]);
+
+        // Sous-catégories de Argent
+        $budget = Categorie::create([
+        	'nom' => 'Budget',
+        	'categorieParente_id' => $argent->id,
+        ]);
+        
+        $consommation = Categorie::create([
+        	'nom' => 'Consommation',
+        	'categorieParente_id' => $argent->id,
+        ]);
+        
+        $dette = Categorie::create([
+        	'nom' => 'Dettes',
+        	'categorieParente_id' => $argent->id,
+        ]);
+        
+        $poursuites = Categorie::create([
+        	'nom' => 'Poursuites',
+        	'categorieParente_id' => $argent->id,
+        ]);
+        
+        // Sous-catégories de Budget
+        $pourquoi = Categorie::create([
+        	'nom' => 'Pourquoi ?',
+        	'categorieParente_id' => $budget->id,
+        ]);
+        
+        $charges = Categorie::create([
+        	'nom' => 'Les charges',
+        	'categorieParente_id' => $budget->id,
+        ]);
+        
+        $revenus = Categorie::create([
+        	'nom' => 'Les revenus',
+        	'categorieParente_id' => $budget->id,
         ]);
 
         $actu1 = new Actualite([
@@ -162,37 +248,37 @@ class RegionPaysSeeder extends Seeder
 		* Seeding Quizz + Badge
         */
         $badge = Badge::create([
-        	'titre' => 'Quizz sida',
-        	'image' => 'badgeSida.png',
+        	'titre' => 'Quizz sur les revenus',
+        	'image' => 'badgeRevenus.png',
         ]);
 		$quizz = new Quizz([
-			'titre' => 'Quizz sur le sida',
+			'titre' => 'Quizz sur les revenus',
 			'date' => date('Y-m-d'),
 			'etat' => 'cache',
 			'badge_id' => $badge->id,
-			'categorie_id' => $sida->id,
+			'categorie_id' => $revenus->id,
 		]);
 		$admin->quizzs()->save($quizz);
 
 		$question = Question::create([
-			'texte' => 'Le sida est-il contagieux ?',
-			'illustration' => 'none.jpg',
+			'texte' => 'Une allocation familiale c\'est quoi ?',
+			'illustration' => 'quizzRevenus.jpg',
 			'quizz_id' => $quizz->id,
 		]);
 
 		$reponse1 = Reponse::create([
-			'texte' => 'Oui',
-			'estJuste' => true,
+			'texte' => 'Donner un argent de poche aux jeunes.',
+			'estJuste' => false,
 			'question_id' => $question->id,
 		]);
 		$reponse2 = Reponse::create([
-			'texte' => 'Non',
-			'estJuste' => false,
+			'texte' => 'Aider les parents financièrement pour ton entretien.',
+			'estJuste' => true,
 			'question_id' => $question->id,
 		]);
 
 		$reponse3 = Reponse::create([
-			'texte' => 'Ne sais pas',
+			'texte' => 'Une taxe imposée à toutes les familles suisses.',
 			'estJuste' => false,
 			'question_id' => $question->id,
 		]);
