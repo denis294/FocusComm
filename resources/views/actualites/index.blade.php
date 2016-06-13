1898785156ee2e42085e23f8d4afa72197a772c7
MrSmon <simon.contrerasaguza@gmail.com>
2016-06-13 11:18:07 +0200

    Actualité
    tableau actualité

--- resources/views/actualites/index.blade.php
+++ resources/views/actualites/index.blade.php
@@ -2,15 +2,14 @@
 @section('title', 'Actualités')
 <link rel="stylesheet" href="{{asset('assets/css/styleactu.css')}}">
 @section('content')
-<h1>Actualtés</h1>
 
 <script>
 var NEWS = {!! $news !!};
 </script>
 
 <section id ="news">
-    <div id="wrapper">
-        <div id ="columns">
+    <div class="container">
+        <div class="row" id="rowactu">
             
         </div>
     </div>                
