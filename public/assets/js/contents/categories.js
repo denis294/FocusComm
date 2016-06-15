a593a29013a9349def54acf86f48cc46908a4ba3
MrSmon <simon.contrerasaguza@gmail.com>
2016-06-15 14:23:10 +0200

    Revert "Catégorie"
    This reverts commit e81b3b6ecd12be8a80a15e304900ef2ea7920701.

--- public/assets/js/contents/categories.js
+++ public/assets/js/contents/categories.js
@@ -5,7 +5,7 @@ var CATEGORIES = [
         "img": "stress.png",
         "id": "1",
         "icone": "f100",
-        "id_lien": "stress",
+        "id_lien": "STRESS",
         niveau2: [
             {"niveau2": "sous-titre"},
             {niveau3: [
@@ -18,7 +18,7 @@ var CATEGORIES = [
         "img": "estime.png",
         "id": "2",
         "icone": "f10C",
-        "id_lien": "estime",
+        "id_lien": "ESTIME",
 
         niveau2: [
             {"niveau2": "sous-titre"},
@@ -35,7 +35,7 @@ var CATEGORIES = [
         "img": "sexualite.png",
         "id": "3",
         "icone": "f107",
-        "id_lien": "sexualite",
+        "id_lien": "SEXUALITE",
         niveau2: [
         {"niveau2": "sous-titre"},
             {niveau3: [
@@ -62,7 +62,7 @@ var CATEGORIES = [
         "img": "boire.png",
         "id": "4",
         "icone": "f10A",
-        "id_lien": "boire",
+        "id_lien": "BOIRE",
         niveau2: [
             {"niveau2": "sous-titre"},
             {niveau3: [
@@ -75,7 +75,7 @@ var CATEGORIES = [
         "img": "sante.png",
         "id": "5",
         "icone": "f105",
-        "id_lien": "sante",
+        "id_lien": "SANTE",
         niveau2: [
             {"niveau2": "sous-titre"},
             {niveau3: [
@@ -88,7 +88,7 @@ var CATEGORIES = [
         "img": "manger.png",
         "id": "6",
         "icone": "f108",
-        "id_lien": "manger",
+        "id_lien": "MANGER",
         niveau2: [
             {"niveau2": "sous-titre"},
             {niveau3: [
@@ -101,7 +101,7 @@ var CATEGORIES = [
         "img": "moi.png",
         "id": "7",
         "icone": "f10D",
-        "id_lien": "moi",
+        "id_lien": "MOI",
         niveau2: [
             {"niveau2": "sous-titre"},
             {niveau3: [
@@ -114,7 +114,7 @@ var CATEGORIES = [
         "img": "violences.png",
         "id": "8",
         "icone": "f10E",
-        "id_lien": "violence",
+        "id_lien": "VIOLENCE",
 
         niveau2: [
             {"niveau2": "sous-titre"},
@@ -128,7 +128,7 @@ var CATEGORIES = [
         "img": "formation.png",
         "id": "9",
         "icone": "f103",
-        "id_lien": "formation",
+        "id_lien": "FORMATION",
         niveau2: [
             {"niveau2": "sous-titre"},
             {niveau3: [
@@ -141,7 +141,7 @@ var CATEGORIES = [
         "img": "argent.png",
         "id": "10",
         "icone": "f106",
-        "id_lien": "argent",
+        "id_lien": "ARGENT",
         niveau2: [
             {"niveau2": "sous-titre"},
             {niveau3: [
@@ -154,7 +154,7 @@ var CATEGORIES = [
         "img": "religion.png",
         "id": "11",
         "icone": "f102",
-        "id_lien": "religion",
+        "id_lien": "RELIGION",
         niveau2: [
             {"niveau2": "sous-titre"},
             {niveau3: [
@@ -167,7 +167,7 @@ var CATEGORIES = [
        "img": "racisme.png",
         "id": "12",
         "icone": "f101",
-        "id_lien": "discrimination",
+        "id_lien": "DISCRIMINATION",
         niveau2: [
             {"niveau2": "sous-titre"},
             {niveau3: [
